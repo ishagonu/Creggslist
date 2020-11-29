b9e0e4a45a1069ba67682eabd04d47c278bd475d
Hubert Chen <hubert94536@yahoo.com>
2020-11-28 17:21:35 -0800

    Fixed models and routes

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,15 +1,9 @@
 const express = require('express')
-const io = require('socket.io')()
 const bodyParser = require('body-parser')
 const accounts = require('./accountsQueries.js')
 const posts = require('./postsQueries.js')
-const http = require('http')
 
 const app = express()
-const server = http.createServer(app)
-io.attach(server)
-require('./socketEvents.js')(io)
-
 var PORT = process.env.PORT || 5000
 
 app.use(bodyParser.json())
@@ -18,15 +12,17 @@ app.use(
     extended: true,
   }),
 )
+
 // if development mode, allow self-signed ssl
 if (app.get('env') === 'development') {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 }
 
 // accounts table
-app.route('/accounts').get(accounts.getAllAccounts).post(accounts.createAccount)
-
-app.route('/accounts/search/:text').get(accounts.searchAccounts)
+app
+  .route('/accounts')
+  .get(accounts.getAllAccounts)
+  .post(accounts.createAccount)
 
 app
   .route('/accounts/:id')
@@ -34,20 +30,33 @@ app
   .put(accounts.updateAccount)
   .delete(accounts.deleteAccount)
 
-app.route('/username/:username').get(accounts.checkUsername)
+app
+  .route('/email/:email')
+  .get(accounts.checkEmail)
 
-app.route('/phoneNumber/:phone_number').get(accounts.checkPhoneNumber)
+app
+  .route('/email/:password')
+  .get(accounts.checkPassword)
 
 // friendships table
-app.route('/posts').post(posts.createPosts)
+app
+  .route('/posts')
+  .post(posts.createPosts)
+  .get(posts.getPosts)
 
-app.route('/posts/:user').get(posts.getPosts)
+app
+  .route('/posts/:user')
+  .get(posts.getPost)
+
+app
+  .route('/posts/search/:text')
+  .get(posts.searchPosts)
 
 app
   .route('/posts/:user/:post')
   .delete(posts.deletePost)
   .put(posts.updatePost)
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`App running on port ${PORT}.`)
 })
