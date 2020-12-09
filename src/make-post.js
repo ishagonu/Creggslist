88f77e9df033d3ae31f0e7ef34c863309e16d9bc
Isha G <igonugun@gmail.com>
2020-12-09 14:00:19 -0800

    Posts store image in firebase on submission

--- src/make-post.js
+++ src/make-post.js
@@ -26,10 +26,7 @@ import {storage} from './firebase.js'
 export default function Make_Post(){
     const [images, setImages] = React.useState([]);
     const maxNumber = 1;
-    const onChange = (imageList, addUpdateIndex) => {
-	console.log(imageList, addUpdateIndex);
-	setImages(imageList);
-    };
+ 
 
     const [item_title, setItemTitle] = React.useState('');
     const [description, setDescription] = React.useState('');
@@ -38,43 +35,66 @@ export default function Make_Post(){
     const [keywords, setKeywords] = React.useState('');
 
     const [redirect, setRedirect] = React.useState(false)
-	const [showItemPreview, setItemPreview] = React.useState(false);
+    const [showItemPreview, setItemPreview] = React.useState(false);
 	
-	//For firebase upload
-	const allInputs = {imgUrl: ''}
+    //For firebase upload
+    const allInputs = {imgUrl: ''}
     const [imageAsFile, setImageAsFile] = useState('')
-	const [imageAsUrl, setImageAsUrl] = useState(allInputs)
-	
-	const handleImageAsFile = (e) => {
-		const image = e.target.files[0]
-		setImageAsFile(imageFile => (image))
-	}
+    const [imageAsUrl, setImageAsUrl] = useState(allInputs)
 
-	const handleFireBaseUpload = e => {
-		e.preventDefault()
-	  console.log('start of upload')
-	  // async magic goes here...
-	  if(imageAsFile === '') {
-		console.error(`not an image, the image file is a ${typeof(imageAsFile)}`)
-	  }
-	  const uploadTask = storage.ref(`/images/${imageAsFile.name}`).put(imageAsFile)
-	  //initiates the firebase side uploading 
-	  uploadTask.on('state_changed', 
-	  (snapShot) => {
-		//takes a snap shot of the process as it is happening
-		console.log(snapShot)
-	  }, (err) => {
-		//catches the errors
-		console.log(err)
-	  }, () => {
-		// gets the functions from storage refences the image storage in firebase by the children
-		// gets the download url then sets the image from firebase as the value for the imgUrl key:
-		storage.ref('images').child(imageAsFile.name).getDownloadURL()
-		 .then(fireBaseUrl => {
-		   setImageAsUrl(prevObject => ({...prevObject, imgUrl: fireBaseUrl}))
-		 })
-	  })
-	  }
+    async function dataURLtoFile(dataurl, filename) {
+	var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+	    bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+	while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+	}
+	return new File([u8arr], filename, {type:mime});
+    }
+    
+    const onChange = (imageList, addUpdateIndex) => {
+	console.log(imageList, addUpdateIndex);
+	setImages(imageList);
+	setImageAsFile('')
+	if (imageList.length >= 1) {
+	    setImgUrl(imageList[0].data_url)
+	}
+    };
+    async function setImgUrl(data_url){
+	await dataURLtoFile(data_url, 'file.img').then(file=>{
+	    setImageAsFile(file)
+	
+	    handleFireBaseUpload(file)
+	})
+
+    }
+
+    async function handleFireBaseUpload(file) {
+	console.log('start of upload')
+	// async magic goes here...
+	if(imageAsFile === '') {
+	    console.error(`not an image, the image file is a ${typeof(file)}`)
+	}
+	const uploadTask = storage.ref(`/images/${file.name}`).put(file)
+	//initiates the firebase side uploading 
+	uploadTask.on('state_changed', 
+		      (snapShot) => {
+			  //takes a snap shot of the process as it is happening
+			  console.log(snapShot)
+		      }, (err) => {
+			  //catches the errors
+			  
+			  console.log(err)
+		      }, () => {
+			  // gets the functions from storage refences the image storage in firebase by the children
+			  // gets the download url then sets the image from firebase as the value for the imgUrl key:
+			  storage.ref('images').child(file.name).getDownloadURL()
+			      .then(function(url){
+				  setImageAsUrl(url)
+				  return
+			      })
+		      })
+	
+    }
    //end of firebase functions
     
     function imagePicked(){
@@ -123,18 +143,21 @@ export default function Make_Post(){
 	return showItemPreview;
     }
 
-    async function makePost(){
+    async function makePost() {
 	if (!validInputs()){return}
 	const keys = keywords.split(',');
 	const email = 'junho.choix10@gmail.com'; // Change this
-	const image_link = 'https://api.time.com/wp-content/uploads/2019/11/gettyimages-459761948.jpg?w=800&quality=85';
 	
-	await PostsApi.createPost(email, keys, image_link, zip, description, parseFloat(price), item_title).then(res =>
-	    setRedirect(true)
-	).catch(err =>{
-	    alert('Oh no! You got egged!')
-	})
+	await PostsApi.createPost(email, keys, imageAsUrl, zip, description, parseFloat(price), item_title).then(res => {
+		setRedirect(true)
+	    }).catch(err =>{
+		alert('Oh no! You got egged!')
+	    })
     }
+					  
+			
+					 
+					
     if (redirect) {
 	return <Redirect push to= '/home' />;
     }else {
@@ -166,18 +189,10 @@ export default function Make_Post(){
 				    {...dragProps}
 				>
 				    Click or Drop here
-				</Button>
+			</Button>
+			    	<Button onClick={()=>uploadHelp()} size='lg' variant='light'>?</Button>
 
-				<form onSubmit={handleFireBaseUpload}>
-       	 		<input 
-				// allows you to reach into your file directory and upload image to the browser
-				//For fire base
-		  		type="file"
-		  		onChange={handleImageAsFile}
-        		/>
-				<button>upload to firebase</button>
-      			</form>
-				<Button onClick={()=>uploadHelp()} size='lg' variant='light'>?</Button>
+			   
 			    </div>
 			    
 			    &nbsp;
@@ -228,8 +243,11 @@ export default function Make_Post(){
 				       onChange={(e) => setKeywords(e.target.value)}
 				/>
 			    </div>
-			    <Button size='lg' class='btn' style={{width: '50%'}} disabled={!validateForm()} onClick={() => makePost()}> Post </Button>
-			    <Button size='lg' class='btn' variant='light' style={{width: '50%'}} disabled={!validateForm()} onClick={() => handleOpenItemInfo()}> Preview </Button>
+		<Button size='lg' class='btn' style={{width: '50%'}} disabled={!validateForm()} onClick={() => makePost()}> Post </Button>
+		<Button size='lg' class='btn' variant='light' style={{width: '50%'}} disabled={!validateForm()} onClick={() => handleOpenItemInfo()}> Preview </Button>
+		
+
+
 			</form>
 			<ReactModal isOpen={showPreview()}>
 			    <Button onClick={()=>setItemPreview(false)}> Close </Button>
